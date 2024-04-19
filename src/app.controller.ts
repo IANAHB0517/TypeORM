d34368c60ac9a55e0ff299717f3845863a604eaa
IANAHB0517 <fromtheedge0517@gmail.com>
2024-04-19 17:02:16 +0900

    oneToone relation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserModel } from './entity/user.entity';
+import { ProfileModel } from './entity/profile.entity';
 
 @Controller()
 export class AppController {
   constructor(
     @InjectRepository(UserModel)
     private readonly userRepository: Repository<UserModel>,
+    @InjectRepository(ProfileModel)
+    private readonly profileRepository: Repository<ProfileModel>,
   ) {}
 
   @Patch('users/:id')
@@ -20,17 +23,44 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
-      title: user.title + '0',
+      // title: user.title + '0',
     });
   }
 
   @Post('users')
   postUser() {
-    return this.userRepository.save({ title: 'test Title' });
+    return this.userRepository.save({});
   }
 
   @Get('users')
   getUsers() {
-    return this.userRepository.find();
+    return this.userRepository.find({
+      order: {
+        id: 'asc',
+      },
+    });
+  }
+
+  @Post('user/profile')
+  async createUserAndProfile() {
+    const user = await this.userRepository.save({
+      email: 'asd@naver.com',
+    });
+
+    const profile = await this.profileRepository.save({
+      profileImg: 'asdf.img',
+      user,
+    });
+
+    return user;
+  }
+
+  @Get('usersAndProfile')
+  async userAndProfile() {
+    return this.userRepository.find({
+      relations: {
+        profile: true,
+      },
+    });
   }
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -3,10 +3,18 @@ import {
   CreateDateColumn,
   Entity,
   Generated,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
+import { ProfileModel } from './profile.entity';
+
+// 컬럼에 들어오는 값을 특정한 값으로 제한 해서 데이터의 Integrity를 유지하고자 할 때 사용한다.
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin',
+}
 
 @Entity()
 export class UserModel {
@@ -23,9 +31,43 @@ export class UserModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // 제목
   @Column()
-  title: string;
+  email: string;
+
+  //   // 제목
+  //   @Column({
+  //     // 데이터베이스에서 인지하는 컬럼 타입
+  //     // 자동으로 유추됨
+  //     // 어노테이션 되는 컬럼의 종류에 따라 자동으로 등록 되지만 특정한 타입으로 생성하고자 할 경우에는 원하는 타입을 명시해 줄 수 있다.
+  //     type: 'varchar',
+  //     // 데이터 베이스에서 사용될 컬럼의 이름
+  //     // 프로퍼티 이름으로 자동 유추 됨
+  //     name: 'title',
+  //     // 값의 길이
+  //     // 입력 할 수 있는 글자의 길이가 300
+  //     length: 300,
+  //     // null 가능여부
+  //     nullable: true,
+  //     // true의 경우 처음 저장할 때만 값 지정 가능
+  //     // 이후 값 변경 불가능
+  //     update: true,
+  //     // find()를 실행할 때 기본으로 값을 불러올지
+  //     // 기본값이 true,
+  //     select: false,
+  //     // 기본 값
+  //     // 아무것도 입력을 안했을 때 기본으로 들어가는 값
+  //     default: 'default value',
+  //     // 칼럼중에서 유일무이한 값이 돼야 하는 값
+  //     unique: false,
+  //   })
+  //   title: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
 
   // 데이터 생성일자
   // 데이터가 생성되는 날짜와 시간이 자동으로 입력됨
@@ -49,4 +91,7 @@ export class UserModel {
   @Column()
   @Generated('uuid')
   additionalId: string;
+
+  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  profile: ProfileModel;
 }
